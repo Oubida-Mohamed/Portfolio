901579fbb5590e64acef9c53e272bc7d884d418a
Oubida-Mohamed <m.oubida11@gmail.com>
2023-08-13 21:52:50 +0100

    6 commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,33 @@
 
 import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import { projects } from "./Data";
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/swiper-bundle.min.css'; // Import Swiper styles
+import 'swiper/css';
+
 const Projects=()=>{
+
+
     return <div className="container d-lg-flex" id="projets" style={{padding:"100px 20px"}}>
-     <div className='w-lg-50'>
+     <Swiper navigation={true}
+      spaceBetween={100}
+      slidesPerView={1}
+      loop={true}
+      pagination={{ clickable: true }}
+      autoplay={true}
+      autoplaySpeed={{ delay: 500 }}
+    >
             {/* <Carousel autoPlay={true} infiniteLoop={true} interval={2000} stopOnHover={true} transitionTime={1000} showThumbs={false}> */}
-                {projects.map((e)=>{return <div className="card" key={e.Id}>
+                {projects.map((e)=>{return <SwiperSlide key={e.Id} className='w-100'>
                     <a href='https://github.com/Oubida-Mohamed/Portfolio' rel="noreferrer" target='_blank' className='w-100 h-80 rounded card-img-top'>
                         <img src={e.Image} alt={e.Name} className='w-100'/>
                     </a>
-                </div>})}
+                </SwiperSlide>})}
             {/* </Carousel> */}
-            </div>
+            </Swiper>
     </div>
+    
 }
 export default Projects;
