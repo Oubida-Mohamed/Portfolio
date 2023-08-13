9237cf2444e942ad02c62dbdc3dbdab5f0470f9e
Oubida-Mohamed <m.oubida11@gmail.com>
2023-08-13 20:49:14 +0100

    5 commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,14 +5,15 @@ import { Carousel } from 'react-responsive-carousel';
 import { projects } from "./Data";
 const Projects=()=>{
     return <div className="container d-lg-flex" id="projets" style={{padding:"100px 20px"}}>
-     
-            <Carousel autoPlay={true} infiniteLoop={true} interval={2000} stopOnHover={true} transitionTime={1000} showThumbs={false}>
+     <div className='w-lg-50'>
+            {/* <Carousel autoPlay={true} infiniteLoop={true} interval={2000} stopOnHover={true} transitionTime={1000} showThumbs={false}> */}
                 {projects.map((e)=>{return <div className="card" key={e.Id}>
                     <a href='https://github.com/Oubida-Mohamed/Portfolio' rel="noreferrer" target='_blank' className='w-100 h-80 rounded card-img-top'>
-                        <img src={e.Image} alt={e.Name}/>
+                        <img src={e.Image} alt={e.Name} className='w-100'/>
                     </a>
                 </div>})}
-            </Carousel>
+            {/* </Carousel> */}
+            </div>
     </div>
 }
-export default Projects
+export default Projects;
