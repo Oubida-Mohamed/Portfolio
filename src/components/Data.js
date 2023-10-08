752f6e9b194fb68e1ebf89aebee1296d61fe4938
Oubida-Mohamed <m.oubida11@gmail.com>
2023-10-08 23:36:34 +0100

    V_F3

--- src/components/Data.js
+++ src/components/Data.js
@@ -22,11 +22,11 @@ export const DataProject = [
     {
         id:"French", Nav:["Accueil", "A propos", "Projets", "Contact"], 
         profil:["Bonjour", "je suis", "Développeur Web Full Stack", "Mon CV"], 
-        description:"Je suis un Développeur Web Full Stack passionné et motivé. <br/>Titulaire d'un diplôme technicien spécialisé en développement digital option web full stack. <br/>Je maîtrise les langages de programmation comme HTML, CSS, JavaScript, PHP et les frameworks comme React et Laravel, maîtrise également les bases de données et les langages associés tels que SQL et MongoDB. J'ai travaillé sur des projets allant de sites web différents.", 
+        description:"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Je suis un Développeur Web Full Stack passionné et motivé.<br/>Titulaire d'un diplôme technicien spécialisé en développement digital option web full stack.<br/>Je maîtrise les langages de programmation comme HTML, CSS, JavaScript, PHP et les frameworks comme React et Laravel, maîtrise également les bases de données et les langages associés tels que SQL et MongoDB.<br/>J'ai réalisé aussi plusieurs projets que vous pouvez trouver en bas de la page.", 
     },
     {
         id:"English", Nav:["Home", "About", "Projects", "Contact"], 
         profil:["Hello", "I am", "Full Stack Web Developer", "My resume"], 
-        description:"I am a passionate and motivated Full Stack Web Developer. <br/>Holding a technician diploma specialized in digital development option web full stack. <br/>I am proficient in programming languages like HTML, CSS, JavaScript, PHP, and frameworks such as React and Laravel. Additionally, I am skilled in working with databases and related languages like SQL and MongoDB. I have experience working on various projects, including different websites.", 
+        description:"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I am a passionate and motivated Full Stack Web Developer. <br/>Holding a technician diploma specialized in digital development option web full stack. <br/>I am proficient in programming languages like HTML, CSS, JavaScript, PHP, and frameworks such as React and Laravel.<br/>Additionally, I am skilled in working with databases and related languages like SQL and MongoDB. <br/>I have experience working on various projects, including different websites.", 
     },
 ];
